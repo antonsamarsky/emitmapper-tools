f91c9763a87c3274203bfb30a3f3bea45f28b99f
asa.idev@gmail.com <asa.idev@gmail.com@baaf3a61-f9d5-181c-cd96-2496a9fea991>
2011-02-18 21:10:43 +0000

    Polishing-up code.
    git-svn-id: http://emitmapper-tools.googlecode.com/svn/trunk/@37 baaf3a61-f9d5-181c-cd96-2496a9fea991

--- MappingDefinitions/DataAttributeManager.cs
+++ MappingDefinitions/DataAttributeManager.cs
@@ -11,18 +11,10 @@ namespace MappingDefinitions
 	/// </summary>
 	public static class DataAttributeManager
 	{
-	/// <summary>
+	    /// <summary>
 		/// The collection of attributes values.
 		/// </summary>
-		private static readonly ConcurrentDictionary<MemberInfo, List<KeyValuePair<string, Type>>> MemberFieldsDescription;
-
-		/// <summary>
-		/// Initializes the <see cref="DataAttributeManager"/> class.
-		/// </summary>
-		static DataAttributeManager()
-		{
-			MemberFieldsDescription = new ConcurrentDictionary<MemberInfo, List<KeyValuePair<string, Type>>>();
-		}
+		private static readonly ConcurrentDictionary<MemberInfo, List<KeyValuePair<string, Type>>> MemberFieldsDescription = new ConcurrentDictionary<MemberInfo, List<KeyValuePair<string, Type>>>();
 
 		/// <summary>
 		/// Gets the fields description.
@@ -33,15 +25,10 @@ namespace MappingDefinitions
 		/// </returns>
 		public static List<KeyValuePair<string, Type>> GetDataMemberDefinition(MemberInfo memberInfo)
 		{
-			return MemberFieldsDescription.GetOrAdd(memberInfo, mi =>
-			{
-				var attributes = Attribute.GetCustomAttributes(memberInfo, typeof(DataMemberAttribute), true).Cast<DataMemberAttribute>();
-
-				return (from attribute in attributes
-								let fieldName = string.IsNullOrEmpty(attribute.FieldName) ? mi.Name : attribute.FieldName
-								let fieldType = attribute.FieldType ?? ((PropertyInfo)mi).PropertyType
-								select new KeyValuePair<string, Type>(fieldName, fieldType)).ToList();
-			});
+			return MemberFieldsDescription.GetOrAdd(memberInfo, mi => (from attribute in Attribute.GetCustomAttributes(memberInfo, typeof(DataMemberAttribute), true).Cast<DataMemberAttribute>()
+			                                                           let fieldName = string.IsNullOrEmpty(attribute.FieldName) ? mi.Name : attribute.FieldName
+			                                                           let fieldType = attribute.FieldType ?? ((PropertyInfo)mi).PropertyType
+			                                                           select new KeyValuePair<string, Type>(fieldName, fieldType)).ToList());
 		}
 	}
 }
--- MappingTest/MapperTest.cs
+++ MappingTest/MapperTest.cs
@@ -216,7 +216,7 @@ namespace MappingTest
 		/// Entities to table mapping collection test.
 		/// </summary>
 		/// <param name="capacity">The capacity.</param>
-		[TestCase(1000000)]
+		[TestCase(10000)]
 		public void EntityToTableMappingCollectionTest(int capacity)
 		{
 			var entities = Enumerable.Range(0, capacity).Select(i => new Entity
@@ -233,25 +233,25 @@ namespace MappingTest
 			// Cold mapping
 			Mapper.MapCollection<Entity, Table>(new List<Entity> { new Entity() });
 
-			var stopWatch = new Stopwatch();
-			stopWatch.Start();
-			GC.Collect();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            GC.Collect();
 
 			var tables = Mapper.MapCollection<Entity, Table>(entities).ToArray();
 
-			stopWatch.Stop();
-			Console.WriteLine(string.Format("Mapping of the collection with {0} elements took: {1} ms. Approx. mapping time per object: {2} sec.", capacity, stopWatch.ElapsedMilliseconds, ((float)stopWatch.ElapsedMilliseconds) / capacity));
+            stopWatch.Stop();
+            Console.WriteLine(string.Format("Mapping of the collection with {0} elements took: {1} ms. Approx. mapping time per object: {2} sec.", capacity, stopWatch.ElapsedMilliseconds, ((float)stopWatch.ElapsedMilliseconds) / capacity));
 
-			for (int i = 0; i < capacity; i++)
-			{
-				var table = tables[i];
-				var entity = entities[i];
+            for (int i = 0; i < capacity; i++)
+            {
+                var table = tables[i];
+                var entity = entities[i];
 
-				Assert.AreEqual(entity.Name, table.Fields["order_name"]);
-				Assert.AreEqual(entity.Number, table.Fields["order_number"]);
-				Assert.AreEqual(entity.Price, table.Fields["order_price"]);
-				Assert.AreEqual(entity.UserName, table.Fields["UserName"]);
-			}
+                Assert.AreEqual(entity.Name, table.Fields["order_name"]);
+                Assert.AreEqual(entity.Number, table.Fields["order_number"]);
+                Assert.AreEqual(entity.Price, table.Fields["order_price"]);
+                Assert.AreEqual(entity.UserName, table.Fields["UserName"]);
+            }
 		}
 	}
 }
